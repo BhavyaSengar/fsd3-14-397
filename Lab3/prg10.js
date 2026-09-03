a85c0ef09e0175fa3523e61765e16f6912575a94
Bhavya Sengar <bhavyahts@gmail.com>
2026-09-03 13:22:04 +0530

    prg10

--- Lab3/prg10.js
+++ Lab3/prg10.js
@@ -1,19 +1,19 @@
 import http from "http";
 import { createReadStream } from "fs";
-const server = http.createServer(async (req, res) => {
+const server = http.createServer((req, res) => {
+  console.log("method:", req.method);
+
   if (req.url === "/" && req.method === "GET") {
     res.end("<h1>Products Details</h1>");
   } else if (req.url === "/products" && req.method === "GET") {
-    res.writeHead(200, {
-      "Content-type": "text/json",
-    });
+    res.writeHead(200, { "content-type": "text/json" });
     const stream = createReadStream("./data/products.json", {
       encoding: "utf-8",
     });
     stream.pipe(res);
   } else {
     res.statusCode = 404;
-    res.end("Not Found");
+    res.end("not found");
   }
 });
-server.listen(3001, () => console.log("Prg10 is running at 3001..."));
+server.listen(3001, () => console.log("prg10 is running at port 3001"));
