fef6b47b376e46d24e2e3cb3970192a32b3c4ab7
Bhavya Sengar <bhavyahts@gmail.com>
2026-09-17 13:58:51 +0530

    Basic Routing with expree server

--- lab5/app.js
+++ lab5/app.js
@@ -1,29 +1,29 @@
-import express from 'express';
+import express from "express";
 
-const app=express();
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("<h1> Hello Express </h1>")
+app.get("/", (req, res) => {
+  res.send("<h1> Hello Express </h1>");
 });
 
-app.get('/about',(req,res)=>{
-    res.send("We are FSD Developer")
-})
+app.get("/about", (req, res) => {
+  res.send("We are FSD Developer");
+});
 
-app.get('/login',(req,res)=>{
-    res.send({msg:`user login`})
-})
+app.post("/login", (req, res) => {
+  res.send({ msg: `user login` });
+});
 
-app.put('/user/update/1',(req,res)=>{
-    res.send({msg:`under update`})
-})
+app.put("/user/update/1", (req, res) => {
+  res.send({ msg: `under update` });
+});
 
-app.delete('/users/1',(req,res)=>{
-    res.send({msg:`remove user 1`})
-})
+app.delete("/users/1", (req, res) => {
+  res.send({ msg: `remove user 1` });
+});
 
-app.use((req,res)=>{
-    res.status(404).send("Not Found")      // it run in series if i write it first than always show 404 error
-})
+app.use((req, res) => {
+  res.status(404).send("Not Found"); // it run in series if i write it first than always show 404 error
+});
 
-app.listen(3333,()=>console.log("Server is Running at 3333"));
+app.listen(3333, () => console.log("Server is Running at 3333"));
